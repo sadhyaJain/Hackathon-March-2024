bedb67dfba05baf8cbab162dd46cbdaa9587df01
unknown <pratham.bhamare15@gmail.com>
2024-03-23 01:59:31 +0530

    added registration form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Login from './Components/Login/Login';
-import Professor from './Components/Professor/Professor';
 
 const App = () => {
   return (
-   <Professor/>
+   <>
+   
+   </>
   );
 }
 
