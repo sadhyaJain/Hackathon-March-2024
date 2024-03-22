afaab0b2bd6cbebc0c5c94592b4d8aa023bbd3c3
vaishvi2811 <vaishvi.khandelwal@gmail.com>
2024-03-23 04:14:37 +0530

    created carousel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './Components/Login/Login';
-
+import Carousel from './Components/Carousel/Carousel';
+import React from 'react';
 function App() {
   return (
-    <>
-    <Login/>
-    </>
+ <Carousel/>
   );
 }
 
