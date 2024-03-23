2af7fe8d3309f300bcc3d172c33acc432db4ff93
Dheer Joisher <dheerjoisher7@gmail.com>
2024-03-23 09:34:10 +0530

    created announcement

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-
+import AnnouncementList from './Components/Announcements/Announcements1';
 
 const App = () => {
   return (
    <>
-
+   <AnnouncementList />
    </>
   );
 }
