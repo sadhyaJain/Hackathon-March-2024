00232a099ebb73647116fb158fa9591bf1ed4adb
sadhyaJain <161031673+sadhyaJain@users.noreply.github.com>
2024-03-23 05:57:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import './App.css';
-<<<<<<< HEAD
-
-=======
->>>>>>> bedb67dfba05baf8cbab162dd46cbdaa9587df01
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <>
-
-    </>
-=======
    <>
    
    </>
->>>>>>> bedb67dfba05baf8cbab162dd46cbdaa9587df01
   );
 }
 
